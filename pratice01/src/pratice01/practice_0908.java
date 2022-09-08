f762374c8393d5c6b7b3aa126a7ba8b11cad6bb4
Jihee Han <hatrix1014@gmail.com>
2022-09-09 00:16:25 +0900

    practice code p.125

---
+++ pratice01/src/pratice01/practice_0908.java
@@ -0,0 +1,16 @@
+package pratice01;
+
+public class practice_0908 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+
+		for (int i = 1; i <= 20; i++) {
+			if (i % 2 != 0 && i % 3 != 0)
+				sum += i;
+		}
+		System.out.println("sum=" + sum);
+
+	}
+
+}
